c6cf5495ef93d5a1852790953ef46d08eb8191dc
Dominic Ho <domtkho@gmail.com>
2014-11-26 20:07:02 +0800

    Fixed friend column but broke drag drop

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141125095901) do
+ActiveRecord::Schema.define(version: 20141126113740) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 20141125095901) do
     t.integer  "item_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "priority"
   end
 
 end
